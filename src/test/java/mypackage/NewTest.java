2a5dea0eb6612e42b60276fcbca70d29029e078e
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-07-26 08:38:04 +0530

    added some code
    dsfsdf

--- src/test/java/mypackage/NewTest.java
+++ src/test/java/mypackage/NewTest.java
@@ -1,6 +1,8 @@
 package mypackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,5 +14,12 @@ public class NewTest {
 	  driver.manage().window().maximize();
 	  System.out.println("Welcome");
 	    System.out.println("Welcome to pune");
+	    WebElement e=driver.findElement(By.id("txtempcode"));
+	    e.sendKeys("CTIE002");
+	    WebElement p=driver.findElement(By.id("txtpassword"));
+	    e.sendKeys("pp");
+	    WebElement btn=driver.findElement(By.id("btnlogin"));
+	    btn.click();
+	    
   }
 }
